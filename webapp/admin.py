c37e844865ac22d3ce88ae602dc2d56f625ba999
Beksultan <sharshenbekov.tech@gmail.com>
2025-06-27 02:39:31 +0600

    Зарегестрировал модель статьи в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from webapp.models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'deadline',)
+    search_fields = ('title',)
+admin.site.register(Article, ArticleAdmin)
