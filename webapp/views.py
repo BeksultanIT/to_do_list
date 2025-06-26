112dc329b75815304d5ff87f0d6a15d428b23fc4
Beksultan <sharshenbekov.tech@gmail.com>
2025-06-27 03:41:03 +0600

    Добавил главную станицу с To_Do_list

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from webapp.models import Article
+
+
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    return render(request, 'index.htm', {'articles': articles})
+
+
