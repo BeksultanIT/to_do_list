bbbc371300b574be30de358cb8f0edbbc28c2ab1
Beksultan <sharshenbekov.tech@gmail.com>
2025-06-27 04:26:16 +0600

    Добавил добавил страницу для добавления заданий

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import index
+from webapp.views import index, new
 
 urlpatterns = [
     path('', index),
+    path('new/', new)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from webapp.models import Article
+from webapp.models import Article, status_choices
 
 
 # Create your views here.
@@ -8,4 +9,12 @@ def index(request):
     articles = Article.objects.all()
     return render(request, 'index.htm', {'articles': articles})
 
-
+def new(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        status = request.POST.get('status')
+        deadline = request.POST.get('deadline')
+        Article.objects.create(title=title, status=status, deadline=deadline)
+        return HttpResponseRedirect("/")
+    else:
+        return render(request, 'new.html', {'status_choices': status_choices})
