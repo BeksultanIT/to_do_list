e7cd0bac67213a37aa13d5b3257d80c513b3bb08
Beksultan <sharshenbekov.tech@gmail.com>
2025-06-27 17:29:18 +0600

    добавили базовую html страничку и подключили bootstrap

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,5 +5,5 @@ from webapp.views import index, new, detail_article
 urlpatterns = [
     path('', index, name='index'),
     path('new/', new, name='new'),
-    path('article<int:pk>/', detail_article, name='detail_article'),
+    path('article/<int:pk>/', detail_article, name='detail_article'),
 ]
