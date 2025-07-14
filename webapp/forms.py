8460f6a94e8c192eb944aabf2c01fe3782d3dd0c
Beksultan <sharshenbekov.tech@gmail.com>
2025-07-14 23:46:57 +0600

    написал формы как модельныеформы

--- webapp/forms.py
+++ webapp/forms.py
@@ -4,12 +4,30 @@ from django.forms import widgets
 from webapp.models import Task, Type, Statuses
 
 
-class TaskForm(forms.Form):
-    title = forms.CharField(max_length=50, required=True, label='Title', widget=widgets.Input(attrs={'class': 'form-control'}))
-    content = forms.CharField( required=True, label='Content', widget=forms.Textarea(attrs={'class': 'form-control'}))
-    deadline = forms.DateField(required=True, label='Deadline',  widget=forms.DateInput(attrs={'type': 'date'}))
-    status = forms.ModelChoiceField(queryset=Statuses.objects.all(), required=True, label='Status', widget=forms.Select(attrs={'class': 'form-control'}))
-    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, label='Types', widget=forms.CheckboxSelectMultiple(attrs={'class': 'custom-checkbox'}))
+
+class TaskForm(forms.ModelForm):
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for v in self.visible_fields():
+            if not isinstance(v.field.widget, widgets.CheckboxSelectMultiple):
+                v.field.widget.attrs['class'] = 'form-control'
+
+
+    deadline = forms.DateField(
+        required=True,
+        label='Deadline',
+        widget=forms.DateInput(attrs={'type': 'date'}))
+
+
+    class Meta:
+        model = Task
+        fields = ['title', 'content', 'deadline','status', 'types',]
+        widgets = {
+            'types': forms.CheckboxSelectMultiple(),
+        }
+
 
 class BulkDeleteForm(forms.Form):
     selected_tasks = forms.ModelMultipleChoiceField(
