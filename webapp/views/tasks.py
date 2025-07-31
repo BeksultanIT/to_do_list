5ab9ab33a0bb986f944836117059d3a53d6a3174
Beksultan <sharshenbekov.tech@gmail.com>
2025-07-31 19:36:29 +0600

    добавил поиск на главную страницу

--- webapp/views/tasks.py
+++ webapp/views/tasks.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlencode
+
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView
@@ -12,13 +15,31 @@ class TaskListView(ListView):
     context_object_name = 'projects'
     paginate_by = 5
 
-    def get_queryset(self):
-        return super().get_queryset()
+    def dispatch(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().dispatch(request, *args, **kwargs)
 
-    def get_context_data(self, * ,object_list = None, **kwargs):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(Q(name__icontains=self.search_value) | Q(description__icontains=self.search_value))
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
         result = super().get_context_data(**kwargs)
-        result['search_form'] = SearchForm()
+        result['search_form'] = self.form
+        if self.search_value:
+            result["query"] = urlencode({"search": self.search_value})
+            result['search'] = self.search_value
         return result
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
 
 
 class CreateTaskView(TemplateView):
